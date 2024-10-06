1de4635cc59f897f7487b6073d974a8810480c7c
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2024-10-07 00:17:12 +0100

    Misc improvements

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-run:
-	. ./.secret.sh && \
+SHELL=/bin/bash
+
+run: venv
+	source ./.secret.sh && \
 		$(VENV)/python ./detector.py
 
 include Makefile.venv
--- start_radiation_detector.sh
+++ start_radiation_detector.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-cd /home/pi/radiation_detector
-source venv/bin/activate
+source ./.venv/bin/activate
 source .secret.sh
 while true ; do
 	./detector.py || true
